fe66d1f9d5f84a9b4a6814ed6bfcdf0818d6adbf
baikarrohit <baikarrohit1999@gmail.com>
2023-11-22 12:22:19 +0530

    Final touch to About US page. worked on Link tag.

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,5 +1,26 @@
+import Link from "next/link";
+
 const AboutUs = () => {
-  return <h1>Welcome to AboutUS page.</h1>;
+  const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
+
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
+
+  return (
+    <div>
+      <h1>Welcome to AboutUS page.</h1>
+      <ul>
+        {details.map((ele) => (
+          <li style={{ fontSize: "25px" }} key={ele.id}>
+            <Link href={`/aboutus/${ele.id}`}>{ele.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default AboutUs;
